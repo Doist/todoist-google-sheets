d769f6d4370d826af5161155ca794629f955cb85
Scott Lovegrove <scott@ferretlabs.com>
2022-08-03 12:32:06 +0100

    tests: Adds tests for succes/error endpoints

--- src/modules/app.e2e-spec.ts
+++ src/modules/app.e2e-spec.ts
@@ -1,4 +1,4 @@
-import { ExtensionVerificationGuard } from '@doist/ui-extensions-server'
+import { ExtensionVerificationGuard, registerViews } from '@doist/ui-extensions-server'
 
 import { Test } from '@nestjs/testing'
 import { getRepositoryToken } from '@nestjs/typeorm'
@@ -28,6 +28,8 @@ async function createTestApp(): Promise<{
 
     const app = moduleRef.createNestApplication<NestExpressApplication>()
 
+    await registerViews(app)
+
     return { appModule: await app.init() }
 }
 
@@ -46,4 +48,16 @@ describe('AppModule', () => {
     it('/GET /ping', () => request(app.getHttpServer()).get('/ping').expect(200).expect('pong'))
 
     it('runs the server without crashing', () => expect(app.getHttpServer()).toBeDefined())
+
+    it(`/GET /success`, () =>
+        request(app.getHttpServer())
+            .get('/success')
+            .expect(200)
+            .expect('Content-Type', /text\/html/))
+
+    it(`/GET /error`, () =>
+        request(app.getHttpServer())
+            .get('/error')
+            .expect(200)
+            .expect('Content-Type', /text\/html/))
 })
