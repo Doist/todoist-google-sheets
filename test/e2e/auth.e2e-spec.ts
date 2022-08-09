3fe417d143708636160d233ff69c6c46b1587d6a
Scott Lovegrove <scott@ferretlabs.com>
2022-08-09 09:21:29 +0100

    refactor: Changes the e2e tests to be split out

---
+++ test/e2e/auth.e2e-spec.ts
@@ -0,0 +1,30 @@
+import request from 'supertest'
+
+import { createTestApp } from './helpers'
+
+import type { INestApplication } from '@nestjs/common'
+
+describe('Auth e2e tests', () => {
+    let app: INestApplication
+
+    afterEach(() => jest.restoreAllMocks())
+
+    afterAll(() => app.close())
+
+    beforeAll(async () => {
+        const { appModule } = await createTestApp()
+        app = appModule
+    })
+
+    it(`/GET /success`, () =>
+        request(app.getHttpServer())
+            .get('/success')
+            .expect(200)
+            .expect('Content-Type', /text\/html/))
+
+    it(`/GET /error`, () =>
+        request(app.getHttpServer())
+            .get('/error')
+            .expect(200)
+            .expect('Content-Type', /text\/html/))
+})
