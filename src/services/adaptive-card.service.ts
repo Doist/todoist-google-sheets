3ac8451c563e8f44d19713536c0974f8f194b08b
Scott Lovegrove <scott@ferretlabs.com>
2022-08-09 09:21:29 +0100

    feat: Adds settings button to logged in home card

--- src/services/adaptive-card.service.ts
+++ src/services/adaptive-card.service.ts
@@ -1,8 +1,12 @@
-import { DoistCard, SubmitAction } from '@doist/ui-extensions-core'
+import { CardElement, DoistCard, SubmitAction } from '@doist/ui-extensions-core'
 import {
     AdaptiveCardService as AdaptiveCardServiceBase,
+    autoColumnSet,
     CardActions,
     Core,
+    createHeader,
+    createIconImage,
+    SETTINGS_IMAGE,
 } from '@doist/ui-extensions-server'
 
 import { Injectable } from '@nestjs/common'
@@ -11,13 +15,42 @@ import { Injectable } from '@nestjs/common'
 export class AdaptiveCardService extends AdaptiveCardServiceBase {
     homeCard(): DoistCard {
         const card = this.createEmptyCard()
-        const action = SubmitAction.from({
-            id: CardActions.LogOut,
-            title: this.translationService.getTranslation(Core.LOGOUT),
+        const header = this.createMainHeader({
+            leftColumnContent: createIconImage(this, {
+                imagePath: SETTINGS_IMAGE,
+                altText: this.translationService.getTranslation(Core.SETTINGS_TITLE),
+                selectAction: SubmitAction.from({
+                    id: CardActions.Settings,
+                }),
+            }),
+            includeEmptySpacing: true,
         })
 
-        card.addAction(action)
+        card.addItem(header)
 
         return card
+    }
+
+    private createMainHeader({
+        leftColumnContent,
+        middleColumnContent,
+        rightColumnContent,
+        includeEmptySpacing = false,
+    }: {
+        leftColumnContent?: CardElement
+        middleColumnContent?: CardElement
+        rightColumnContent?: CardElement
+        includeEmptySpacing?: boolean
+    } = {}): CardElement {
+        const leftColumn = leftColumnContent ? [leftColumnContent] : []
+        const middleColumn = middleColumnContent ? [middleColumnContent] : []
+        const rightColumn = rightColumnContent ? [rightColumnContent] : []
+        return createHeader(
+            this,
+            leftColumn,
+            middleColumn,
+            [autoColumnSet(rightColumn)],
+            includeEmptySpacing,
+        )
     }
 }
