05a3c6773f498e6a63f5f0a0fbe4933df46d3530
Scott Lovegrove <scott@ferretlabs.com>
2022-09-13 15:43:57 +0100

    tests: Adds tests for settings extension

--- test/e2e/core.e2e-spec.ts
+++ test/e2e/core.e2e-spec.ts
@@ -1,11 +1,12 @@
 import request from 'supertest'
 
-import { UserDatabaseService } from '../../src/services/user-database.service'
+import { setupGetUser } from '../setups'
 
 import { createTestApp } from './helpers'
 
-import type { DoistCardResponse } from '@doist/ui-extensions-core'
+import type { DoistCardExtensionType, DoistCardResponse } from '@doist/ui-extensions-core'
 import type { INestApplication } from '@nestjs/common'
+import type { User } from '../../src/entities/user.entity'
 
 describe('Core e2e tests', () => {
     let app: INestApplication
@@ -38,7 +39,10 @@ describe('Core e2e tests', () => {
             .expect(200)
             .expect('Content-Type', /text\/html/))
 
-    it('returns the project only card when coming from a task', () => {
+    it('returns the project only card when coming from a task and signed in', () => {
+        setupGetUser({
+            authToken: 'kwijibo',
+        } as User)
         return request(app.getHttpServer())
             .post('/process')
             .send({
@@ -59,27 +63,59 @@ describe('Core e2e tests', () => {
             })
     })
 
-    it('returns the login card when coming from a project (assuming first run)', () => {
-        jest.spyOn(UserDatabaseService.prototype, 'getUser').mockImplementation(() =>
-            Promise.resolve(undefined),
-        )
+    test.each<DoistCardExtensionType>(['context-menu', 'settings'])(
+        'returns the login card (assuming first run)',
+        (extensionType: DoistCardExtensionType) => {
+            setupGetUser(undefined)
+            return request(app.getHttpServer())
+                .post('/process')
+                .send({
+                    context: { user: { id: 42 }, theme: 'light' },
+                    action: {
+                        actionType: 'initial',
+                        params:
+                            extensionType === 'context-menu'
+                                ? {
+                                      source: 'project',
+                                  }
+                                : undefined,
+                    },
+                    extensionType,
+                })
+                .expect(200)
+                .expect('Content-Type', /json/)
+                .then((response) => {
+                    const body = response.body as DoistCardResponse
+                    expect(body.card).toBeDefined()
+                    expect(JSON.stringify(body)).toMatch(/Log in with Google/)
+                })
+        },
+    )
+
+    it('returns the settings card if the user is logged in and is settings extension', () => {
+        setupGetUser({
+            emailAddress: 'alan@ingen.com',
+            name: 'Alan Grant',
+            authToken: 'kwijibo',
+        } as User)
+
         return request(app.getHttpServer())
             .post('/process')
             .send({
                 context: { user: { id: 42 }, theme: 'light' },
                 action: {
                     actionType: 'initial',
-                    params: {
-                        source: 'project',
-                    },
                 },
+                extensionType: 'settings',
             })
             .expect(200)
             .expect('Content-Type', /json/)
             .then((response) => {
                 const body = response.body as DoistCardResponse
                 expect(body.card).toBeDefined()
-                expect(JSON.stringify(body)).toMatch(/Log in with Google/)
+                expect(JSON.stringify(body)).toMatch(
+                    /You’re connected to Google as \*\*Alan Grant\*\* \(alan@ingen.com\)./,
+                )
             })
     })
 })
